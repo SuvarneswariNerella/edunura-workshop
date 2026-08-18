9a51d4b9dc3ce3f25ffb6406fa5a948e09d3b611
SuvarneswariNerella <suvarneswari20@gmail.com>
2026-08-18 15:34:30 +0530

    feat: add Dockerfile and prepare for Dokploy deployment

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import fs from 'fs';
@@ -5,7 +6,7 @@ import nodemailer from 'nodemailer';
 import { createServer as createViteServer } from 'vite';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.use(express.json());
 
